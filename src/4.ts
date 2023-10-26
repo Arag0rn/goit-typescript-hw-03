e841212d12a66a83bab73efce19af5c5bbbdfce1
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-10-26 12:27:38 +0200

    finished HW

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+
+
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+
+  abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+        this.key = key;
+        this.door = false; 
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            }
+        }
+      
+  
+    public abstract openDoor(key: Key): void;
+
+  }
+  
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -9,3 +64,5 @@ house.comeIn(person);
 
 
 export {};
+
+
